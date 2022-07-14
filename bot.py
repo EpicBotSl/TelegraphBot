b8b81cdd30f620e4534c32db2b2aae1077aef814
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-14 15:29:34 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import os
+from config.import
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import logging
+
+app = Client(
+    "Team Epic",
+    api_id=("API_ID"),
+    api_hash=("API_HASH"),
+    bot_token=(BOT_TOKEN)
+)
+
+@app.on_message(filters.command("webss"))
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text(
+                "Give A Url To Fetch Screenshot."
+            )
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
+        try:
+            await message.reply_photo(
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
+            )
+        except TypeError:
+            return await m.edit("No Such Website.")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
+
+print("hellow world")
+app.run()
