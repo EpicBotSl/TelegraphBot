1e133d6bb0637cf9e0dc769b39687d197704ae5b
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-15 11:22:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,75 @@ import logging
 from telegraph import upload_file
 from urllib.parse import quote
 import asyncio
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+import random
+
+
+
+@Client.on_message(filters.photo)
+async def uploadphoto(client, message):
+  msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join` @septemberfilms")
+  userid = str(message.chat.id)
+  img_path = (f"./Download....!/{userid}.jpg")
+  img_path = await client.download_media(message=message, file_name=img_path)
+  await msg.edit_text("`Uploading..... join` @septemberfilms")
+  try:
+    tlink = upload_file(img_path)
+  except:
+    await msg.edit_text("`Something went wrong join` @septemberfilms") 
+  else:
+    await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")  
+    reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("OPEN LINK", url=f"https://telegra.ph{tlink[0]}"),
+                 InlineKeyboardButton("DEV", url=f"t.me/devourdevils"),
+                 ],[
+                 InlineKeyboardButton("GROUP", url=f"t.me/septemberfilms")
+                 ]]
+                )
+    os.remove(img_path) 
+    
+            
+
+@Client.on_message(filters.animation)
+async def uploadgif(client, message):
+  if(message.animation.file_size < 5242880):
+    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join` @septemberfilms")
+    userid = str(message.chat.id)
+    gif_path = (f"./DOWNLOADS/{userid}.mp4")
+    gif_path = await client.download_media(message=message, file_name=gif_path)
+    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+    try:
+      tlink = upload_file(gif_path)
+      await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")   
+      os.remove(gif_path)   
+    except:
+      await msg.edit_text("Something really Happend Wrong... join @septemberfilms") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb join @septemberfilms")
+
+@Client.on_message(filters.video)
+async def uploadvid(client, message):
+  if(message.video.file_size < 5242880):
+    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join` @septemberfilms")
+    userid = str(message.chat.id)
+    vid_path = (f"./DOWNLOADS/{userid}.mp4")
+    vid_path = await client.download_media(message=message, file_name=vid_path)
+    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+    try:
+      tlink = upload_file(vid_path)
+      await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")     
+      os.remove(vid_path)   
+    except:
+      await msg.edit_text("Something really Happend Wrong... join @septemberfilms") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb join @septemberfilms")
 
 app = Client(
     "Team Epic",
