e7e1cfcbe8c3272a4a716c1ca4e7bffed7137c22
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-15 14:28:38 +0530

    Create tganim.py

---
+++ module/tganim.py
@@ -0,0 +1,43 @@
+import os
+from config import *
+from pyrogram import *
+from pyrogram.types import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import logging
+from telegraph import upload_file
+from urllib.parse import quote
+import asyncio
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+import random
+
+app = Client
+
+@app.on_message(filters.animation)
+async def uploadgif(client, message):
+  if(message.animation.file_size < 5242880):
+    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join **@EpicChats**")
+    userid = str(message.chat.id)
+    gif_path = (f"./DOWNLOADS/{userid}.mp4")
+    gif_path = await client.download_media(message=message, file_name=gif_path)
+    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
+    try:
+      tlink = upload_file(gif_path)
+      await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")   
+      os.remove(gif_path)   
+    except:
+      await msg.edit_text("Something really Happend Wrong... join **@EpicChats**") 
+  else:
+    await message.reply_text("Size Should Be Less Than 5 mb join **@EpicChats**")
+
+print(f"""
+Animation section powerd Up!...
+""")
