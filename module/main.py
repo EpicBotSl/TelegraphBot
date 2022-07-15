faec635f0adcb4f891c50c9ebb6d893698cb1a5e
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-15 16:06:57 +0530

    Create main.py

---
+++ module/main.py
@@ -0,0 +1,106 @@
+import re
+import uuid
+import socket
+import platform
+import os
+import random
+import time
+import math
+import json
+import string
+import traceback
+import psutil
+import asyncio
+import wget
+import motor.motor_asyncio
+import pymongo
+import aiofiles
+import datetime
+from pyrogram.errors.exceptions.bad_request_400 import *
+from pyrogram.errors import *
+from pyrogram import Client, filters
+from pyrogram.errors import *
+from pyrogram.types import *
+from helper.decorators import humanbytes
+from config import *
+from database.db import Database
+from asyncio import *
+import heroku3
+import requests
+from script import *
+from helper.heroku_helper import HerokuHelper
+from helper.fsub import forcesub
+from database.check_user import *
+#--------------------------------------------------Db-------------------------------------------------#
+
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : user is blocked\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        
+#------------------------------Db End---------------------------------------------------------#       
+
+DATABASE_URL=MONGO_URI
+db = Database(DATABASE_URL, "telegraph_bot")
+
+#╔════╗────────╔═══╗
+#║╔╗╔╗║────────║╔══╝
+#╚╝║║╠╩═╦══╦╗╔╗║╚══╦══╦╦══╗
+#──║║║║═╣╔╗║╚╝║║╔══╣╔╗╠╣╔═╝
+#──║║║║═╣╔╗║║║║║╚══╣╚╝║║╚═╗
+#──╚╝╚══╩╝╚╩╩╩╝╚═══╣╔═╩╩══╝
+#──────────────────║║
+#──────────────────╚╝
+#---------------------------Commands Start Epic-------------------------------------#
+
+client = Client
+
+@Client.on_message(filters.command("start"))
+async def start(client, message):
+    if await forcesub(client, message):
+       return
+    chat_id = message.from_user.id
+    if not await db.is_user_exist(chat_id):
+        data = await client.get_me()
+        BOT_USERNAME = data.username
+        await db.add_user(chat_id)
+        if LOG_CHANEL:
+            await client.send_message(
+                LOG_CHANEL,
+                f"#NEWUSER: \n\n**User:** [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n\**ID:**{message.from_user.id}\n Started @{BOT_USERNAME} !!",
+            )
+        else:
+            logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
+    await message.delete()
+    file_id = "CAADBQADbgUAAsyaaFZB8CvoDrUN_AI"
+    await client.send_sticker(message.chat.id, file_id)
+    text = f"""
+      ❣ʜɪ {message.from_user.mention} 
+   ⏦ɪ ᴀᴍ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ɢᴇɴᴇʀᴇᴛᴏʀ ʙᴏᴛ ⏦
+🍁ɪ ᴄᴀɴ ɢᴇɴᴇʀᴇʀᴀᴛᴇ :
+        🌀**ᴘʏʀᴏɢʀᴀᴍ**
+        🌀**ᴛᴇʟᴇᴛʜᴏɴ**
+        🌀**ᴘʏʀᴏɢʀᴀᴍ ᴠ2**
+ꜱᴇꜱꜱɪᴏɴꜱ✓
+🌍ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ
+☔ᴘʀᴏᴊᴇᴄᴛ ʙʏ : [ᴇᴘɪᴄ ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ](https://t.me/EpicBotsSl)"""
+    reply_markup = START_BUTTON  
+    await message.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
