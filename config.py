0ea8ee08f30f48525ffba47742a4bb1b63cd845f
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-15 15:55:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ from pyrogram.types import *
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+MONGO_URI = os.getenv("MONGO_URI", "").strip()
+AUTH_CHANNEL = os.getenv("AUTH_CHANNEL", "").strip()
+BOT_USERNAME = os.getenv("BOT_USERNAME", "").strip()
+AUTH_USERS = os.getenv("AUTH_USERS", "").strip()
