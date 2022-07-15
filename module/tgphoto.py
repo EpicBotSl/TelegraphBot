2cf463ddd6d7c5a1c768563930550ad63fff01a1
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-15 15:26:47 +0530

    Update tgphoto.py

--- module/tgphoto.py
+++ module/tgphoto.py
@@ -120,27 +120,6 @@ async def uploadphoto(client, message):
 ➥  `https://telegra.ph{tlink[0]}`
 """) 
     os.remove(img_path) 
-    
-            
-
-
-
-@app.on_message(filters.video)
-async def uploadvid(client, message):
-  if(message.video.file_size < 5242880):
-    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join` @septemberfilms")
-    userid = str(message.chat.id)
-    vid_path = (f"./DOWNLOADS/{userid}.mp4")
-    vid_path = await client.download_media(message=message, file_name=vid_path)
-    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....`")
-    try:
-      tlink = upload_file(vid_path)
-      await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")     
-      os.remove(vid_path)   
-    except:
-      await msg.edit_text("Something really Happend Wrong... join @septemberfilms") 
-  else:
-    await message.reply_text("Size Should Be Less Than 5 mb join @septemberfilms")
 
 
 @app.on_message(filters.command("webss"))
